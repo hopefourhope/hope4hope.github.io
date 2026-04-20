561a72279aeebb69df6ecdf5627d917b1c22c64a
Soph-stara <sophie-maximiliane.hamann@student.uni-ak.ac.at>
2026-04-20 17:44:28 +0200

    links links links

---
+++ work_descriptions/data.js
@@ -0,0 +1,37 @@
+const artistModes = {
+    "work-1":  ["coping", "care", "force", "material", "memory", "passing-on", "persistence", "resistance", "dynamic", "collective-emotion", "fragility"],
+    "work-2":  ["attitude", "care", "compassion", "contradiction", "coping", "fragility", "memory", "resilience", "speculation", "strategy", "trust", "uncertainty", "wish"],
+    "work-3":  ["action", "agency", "material", "speculation", "strategy"],
+    "work-4":  ["action", "agency", "attitude", "force", "fragility", "freedom", "trust", "uncertainty", "utopia", "wish"],
+    "work-5":  ["agency", "collective-emotion", "force", "momentum", "resistance", "strategy"],
+    "work-6":  ["action", "agency", "alternative", "resistance"],
+    "work-7":  ["absence-of-fear", "action", "agency", "collective-emotion", "dynamic", "emerging", "freedom", "momentum", "persistence", "resilience", "resistance", "self-organization", "uncertainty"],
+    "work-8":  ["contradiction", "embodiment", "fragility", "persistence", "resilience", "resistance", "tension"],
+    "work-9":  ["contradiction", "persistence", "uncertainty"],
+    "work-10": ["force", "momentum", "trust"],
+    "work-11": ["action", "agency", "care", "coping", "material", "persistence", "self-organization"],
+    "work-12": ["agency", "illusion", "persistence", "speculation", "uncertainty"],
+    "work-13": ["alternative", "collective-emotion", "embodiment", "freedom", "memory", "momentum", "resistance"],
+    "work-14": ["absence-of-fear", "action", "agency", "force", "horror", "illusion", "poison", "uncertainty", "wish"],
+    "work-15": ["care", "collective-care-solidarity", "collective-emotion", "compassion", "contradiction", "embodiment", "fragility", "resilience"],
+    "work-16": []
+};
+
+const workMeta = {
+    "work-1":  { title: "L  E   F  T | F  E  L   T", artist: "Khadisha Dabayeva", file: "works/work-1.html" },
+    "work-2":  { title: "Staying in Touch 2.0", artist: "Lili Kátai and Elias Schulz", file: "works/work-2.html" },
+    "work-3":  { title: "The Grammar of Agency", artist: "Yiwen Che", file: "works/work-3.html" },
+    "work-4":  { title: "hopestars: werden", artist: "the funky siblings", file: "works/work-4.html" },
+    "work-5":  { title: "Facing It", artist: "Marshall Paul", file: "works/work-5.html" },
+    "work-6":  { title: "How to resist tech-bros", artist: "Benjamin Palme", file: "works/work-6.html" },
+    "work-7":  { title: "The Black Box of Hope", artist: "Subham Manandhar", file: "works/work-7.html" },
+    "work-8":  { title: "Resignation and the Refusal to Resign", artist: "Sophie Hamann", file: "works/work-8.html" },
+    "work-9":  { title: "Tentative d'épuisement", artist: "Marlene Lethmayer", file: "works/work-9.html" },
+    "work-10": { title: "Rocks, I mean; - Stars", artist: "Ádám Salomvári and Laura Oyuela", file: "works/work-10.html" },
+    "work-11": { title: "Inventory of Survival", artist: "Alessia Meyer", file: "works/work-11.html" },
+    "work-12": { title: "unknown return to player", artist: "Lea Kristin Würtenberger", file: "works/work-12.html" },
+    "work-13": { title: "If My Land Were Flat, None of This Would Have Happened", artist: "Sara Karimi", file: "works/work-13.html" },
+    "work-14": { title: "SHELLS", artist: "Charlie Spies", file: "works/work-14.html" },
+    "work-15": { title: "Hope is a Practice", artist: "Emilie Schmidt", file: "works/work-15.html" },
+    "work-16": { title: "Work Title 16", artist: "Artist Name", file: "works/work-16.html" }
+};
