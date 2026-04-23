7d8ca8d5833304d8c767e3b650e653738c46fb5c
Soph-stara <sophie-maximiliane.hamann@student.uni-ak.ac.at>
2026-04-23 15:00:00 +0200

    fixing bugs

--- data.js
+++ data.js
@@ -18,20 +18,20 @@ const artistModes = {
 };
 
 const workMeta = {
-    "work-1":  { title: "L  E   F  T | F  E  L   T", artist: "Khadisha Dabayeva", file: "works/work-1.html" },
-    "work-2":  { title: "Staying in Touch 2.0", artist: "Lili Kátai and Elias Schulz", file: "works/work-2.html" },
-    "work-3":  { title: "The Grammar of Agency", artist: "Yiwen Che", file: "works/work-3.html" },
-    "work-4":  { title: "hopestars: werden", artist: "the funky siblings", file: "works/work-4.html" },
-    "work-5":  { title: "Facing It", artist: "Marshall Paul", file: "works/work-5.html" },
-    "work-6":  { title: "How to resist tech-bros", artist: "Benjamin Palme", file: "works/work-6.html" },
-    "work-7":  { title: "The Black Box of Hope", artist: "Subham Manandhar", file: "works/work-7.html" },
-    "work-8":  { title: "Resignation and the Refusal to Resign", artist: "Sophie Hamann", file: "works/work-8.html" },
-    "work-9":  { title: "Tentative d'épuisement", artist: "Marlene Lethmayer", file: "works/work-9.html" },
-    "work-10": { title: "Rocks, I mean; - Stars", artist: "Ádám Salomvári and Laura Oyuela", file: "works/work-10.html" },
-    "work-11": { title: "Inventory of Survival", artist: "Alessia Meyer", file: "works/work-11.html" },
-    "work-12": { title: "unknown return to player", artist: "Lea Kristin Würtenberger", file: "works/work-12.html" },
-    "work-13": { title: "If My Land Were Flat, None of This Would Have Happened", artist: "Sara Karimi", file: "works/work-13.html" },
-    "work-14": { title: "SHELLS", artist: "Charlie Spies", file: "works/work-14.html" },
-    "work-15": { title: "Hope is a Practice", artist: "Emilie Schmidt", file: "works/work-15.html" },
-    "work-16": { title: "Work Title 16", artist: "Artist Name", file: "works/work-16.html" }
+    "work-1":  { title: "L  E   F  T | F  E  L   T", artist: "Khadisha Dabayeva", file: "work_descriptions/work-1.html" },
+    "work-2":  { title: "Staying in Touch 2.0", artist: "Lili Kátai and Elias Schulz", file: "work_descriptions/work-2.html" },
+    "work-3":  { title: "The Grammar of Agency", artist: "Yiwen Che", file: "work_descriptions/work-3.html" },
+    "work-4":  { title: "hopestars: werden", artist: "the funky siblings", file: "work_descriptions/work-4.html" },
+    "work-5":  { title: "Facing It", artist: "Marshall Paul", file: "work_descriptions/work-5.html" },
+    "work-6":  { title: "How to resist tech-bros", artist: "Benjamin Palme", file: "work_descriptions/work-6.html" },
+    "work-7":  { title: "The Black Box of Hope", artist: "Subham Manandhar", file: "work_descriptions/work-7.html" },
+    "work-8":  { title: "Resignation and the Refusal to Resign", artist: "Sophie Hamann", file: "work_descriptions/work-8.html" },
+    "work-9":  { title: "Tentative d'épuisement", artist: "Marlene Lethmayer", file: "work_descriptions/work-9.html" },
+    "work-10": { title: "Rocks, I mean; - Stars", artist: "Ádám Salomvári and Laura Oyuela", file: "work_descriptions/work-10.html" },
+    "work-11": { title: "Inventory of Survival", artist: "Alessia Meyer", file: "work_descriptions/work-11.html" },
+    "work-12": { title: "unknown return to player", artist: "Lea Kristin Würtenberger", file: "work_descriptions/work-12.html" },
+    "work-13": { title: "If My Land Were Flat, None of This Would Have Happened", artist: "Sara Karimi", file: "work_descriptions/work-13.html" },
+    "work-14": { title: "SHELLS", artist: "Charlie Spies", file: "work_descriptions/work-14.html" },
+    "work-15": { title: "Hope is a Practice", artist: "Emilie Schmidt", file: "work_descriptions/work-15.html" },
+    "work-16": { title: "Work Title 16", artist: "Artist Name", file: "work_descriptions/work-16.html" }
 };
